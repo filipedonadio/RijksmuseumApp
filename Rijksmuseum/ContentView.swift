2516c57face5925806b8e09a03e4eeec86e46a33
Filipe Donadio <filipe.donadio@gmail.com>
2023-05-13 19:45:08 +0200

    Create a new Xcode project

---
+++ Rijksmuseum/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rijksmuseum
+//
+//  Created by Filipe Donadio on 13/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rijksmuseum/RijksmuseumApp.swift
@@ -0,0 +1,17 @@
+//
+//  RijksmuseumApp.swift
+//  Rijksmuseum
+//
+//  Created by Filipe Donadio on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RijksmuseumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
