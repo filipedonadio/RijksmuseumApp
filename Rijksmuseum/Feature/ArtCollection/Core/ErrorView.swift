e1afe95da51db8ccc664f38d50702bd67aaaea11
Filipe Donadio <filipe.donadio@gmail.com>
2023-05-14 11:08:13 +0200

    Implement the error view

---
+++ Rijksmuseum/Feature/ArtCollection/Core/ErrorView.swift
@@ -0,0 +1,50 @@
+//
+//  ErrorView.swift
+//  Rijksmuseum
+//
+//  Created by Filipe Donadio on 14/05/2023.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let tryAgainAction: () -> Void
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Image(systemName: "exclamationmark.circle")
+                .font(.system(size: 80))
+                .fontWeight(.light)
+                .foregroundColor(.theme.primaryTextColor)
+                .padding(.bottom, 32)
+            Text("Oh no!")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 8)
+            Text("Something went wrong. Please try again.")
+                .foregroundColor(.theme.secondaryTextColor)
+                .font(.body)
+            Spacer()
+            Button("Try again") {
+                tryAgainAction()
+            }
+            .padding(.vertical, 16)
+            .padding(.horizontal, 40)
+            .background(Color.theme.accent)
+            .cornerRadius(8)
+            .foregroundColor(.white)
+            .font(.body)
+            .fontWeight(.bold)
+            .padding(.bottom, 32)
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.theme.backgroundElevatedColor)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView(tryAgainAction: {})
+    }
+}
