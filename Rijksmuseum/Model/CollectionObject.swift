53bb75871f74b79c7d28208dae1c16bc288063eb
Filipe Donadio <filipe.donadio@gmail.com>
2023-05-14 19:00:00 +0200

    Make CollectionObject conform to Hashable

--- Rijksmuseum/Model/CollectionObject.swift
+++ Rijksmuseum/Model/CollectionObject.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct CollectionObject: Decodable, Identifiable {
+struct CollectionObject: Decodable, Identifiable, Hashable {
     let objectNumber: String
     let title: String
     let principalOrFirstMaker: String
@@ -31,4 +31,23 @@ struct CollectionObject: Decodable, Identifiable {
             isFavorite: !isFavorite
         )
     }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(objectNumber)
+        hasher.combine(title)
+    }
+    
+    static func == (lhs: CollectionObject, rhs: CollectionObject) -> Bool {
+        lhs.objectNumber == rhs.objectNumber && lhs.title == rhs.title
+    }
+}
+
+extension CollectionObject {
+    static let testData = CollectionObject(
+        objectNumber: "en-SK-A-1718",
+        title: "Winter Landscape with Ice Skaters",
+        principalOrFirstMaker: "Hendrick Avercamp",
+        webImage: WebImage.testData,
+        isFavorite: false
+    )
 }
