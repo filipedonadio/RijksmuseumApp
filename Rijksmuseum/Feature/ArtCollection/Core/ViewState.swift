1966a6930d376300218b0353336247cd2b2fe9a2
Filipe Donadio <filipe.donadio@gmail.com>
2023-05-14 11:13:42 +0200

    Create an enum to represent the view state

---
+++ Rijksmuseum/Feature/ArtCollection/Core/ViewState.swift
@@ -0,0 +1,23 @@
+//
+//  ViewState.swift
+//  Rijksmuseum
+//
+//  Created by Filipe Donadio on 14/05/2023.
+//
+
+import Foundation
+
+/// Enum representing the view state.
+public enum ViewState<T> {
+    /// There are no elements in the view.
+    case empty
+    
+    /// The view is loading.
+    case loading
+    
+    /// An error has occurred.
+    case error
+    
+    /// The elements are loaded and ready for use.
+    case success(elements: [T])
+}
