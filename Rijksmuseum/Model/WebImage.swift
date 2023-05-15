5891751de98573aab817fd9baeca45f8ecbac6a7
Filipe Donadio <filipe.donadio@gmail.com>
2023-05-15 09:04:56 +0200

    Remove unnecessary property

--- Rijksmuseum/Model/WebImage.swift
+++ Rijksmuseum/Model/WebImage.swift
@@ -8,13 +8,11 @@
 import Foundation
 
 struct WebImage: Decodable {
-    let guid: String
     let url: String
 }
 
 extension WebImage {
     static let testData = WebImage(
-        guid: "4f834f63-3aeb-48a2-8a22-bcac12ffc3b9",
         url: "https://lh3.googleusercontent.com/1pTfYJlLwVTifKj4PlsWPyAg4PcIVBAiVvB8sameSnmm7HRd056abNUIRq33rgry7u9t-ju-eHOnbfqQpK4q_8IwzIXZ4WgrqZW9l7U=s0"
     )
 }
